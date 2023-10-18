fec52ab6e1f1ad77110b63e25ede24166fc84f33
Kağan Efe <efekaan267@gmail.com>
2023-10-18 20:06:22 +0300

    Service
    Service Dto ve Listeleme işlemleri yapıldı.

---
+++ Frontend/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,30 @@
+﻿using HotelProject.WebUI.DTOs.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+
+        public ServiceController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClient.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5073/api/Service");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/DTOs/ServiceDto/ResultServiceDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.DTOs.ServiceDto
+{
+    public class ResultServiceDto
+    {
+        public int ServiceId { get; set; }
+        [Required(ErrorMessage = "Hizmet ikon linkini giriniz")]
+        public string ServiceRoom { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Hizmet başlığı giriniz")]
+        [StringLength(100, ErrorMessage = "En fazla 100 karakter girilebilir.")]
+        public string Title { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Hizmet açıklaması giriniz")]
+        [StringLength(500, ErrorMessage = "En fazla 500 karakter girilebilir.")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
