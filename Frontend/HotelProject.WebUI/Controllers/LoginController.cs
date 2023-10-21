2e974acd9fc97ef4c4e4145f51d1e7e46f318908
Kağan Efe <efekaan267@gmail.com>
2023-10-21 22:12:23 +0300

    Login ve Register
    Login ve Register sayfaları tasarımları eklendi ve arka kısımları yazıldı

---
+++ Frontend/HotelProject.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.DTOs.LoginDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginUserDto loginUserDto)
+        {
+            var result = await _signInManager.PasswordSignInAsync(loginUserDto.Username, loginUserDto.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Staff");
+            }
+            return View();
+        }
+
+    }
+}
--- Frontend/HotelProject.WebUI/Mapping/AutoMappingConfig.cs
+++ Frontend/HotelProject.WebUI/Mapping/AutoMappingConfig.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.DTOs.LoginDto;
 using HotelProject.WebUI.DTOs.RegisterDto;
 using HotelProject.WebUI.DTOs.ServiceDto;
 
@@ -13,6 +14,7 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
             CreateMap<CreateServiceDto, Service>().ReverseMap();
             CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
+            CreateMap<LoginUserDto, AppUser>().ReverseMap();
         }
     }
 }
